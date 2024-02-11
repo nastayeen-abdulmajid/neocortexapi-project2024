6605938818b2bb52ee45edd87450831c51092760
Nastayeen <nastayeen.abdul-majid@stud.fra-uas.de>
2024-02-11 18:29:56 +0100

    Created a new class to for unit test cases

---
+++ source/UnitTestsProject/UnitTest_AdaptSegment_teamNV.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Damir Dobric. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+using NeoCortexApi;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+
+namespace UnitTestsProject
+{
+    public class UnitTest_AdaptSegment_teamNV : IHtmAlgorithm<int[], ComputeCycle>/*, ISerializable*///: IComputeDecorator
+    {
+
+
+
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ComputeCycle Compute(int[] input, bool learn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(IHtmModule other)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+    }
+}
