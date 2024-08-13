cc01cc8952d599f79f48d96e1caa1dd4668cfbef
Vikas Rathore <vikas.rathore@stud.fra-uas.de>
2024-08-14 01:58:04 +0200

    Implemented InitLogging method in InitHelpers to configure logging infrastructure

---
+++ Cloud Project 2024/MyCloudProjectSample/MyCloudProject.Common/InitHelpers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.Extensions.Logging.Console;
+
+namespace MyCloudProject.Common
+{
+    public static class InitHelpers
+    {
+        /// <summary>
+        /// Create Logging infrastructure in the Trainer Workload.
+        /// Todo: Make sure that the logging level can be configured on start of the application.
+        /// </summary>
+        /// <returns></returns>
+        public static ILoggerFactory InitLogging(IConfigurationRoot configRoot)
+        {
+            //create logger from the appsettings addConsole Debug to Logg 
+            return LoggerFactory.Create(logBuilder =>
+            {
+                ConsoleLoggerOptions logCfg = new ConsoleLoggerOptions();
+
+                logBuilder.AddConfiguration(configRoot.GetSection("Logging"));
+
+                logBuilder.AddConsole((opts) =>
+                {
+                    opts.IncludeScopes = true;
+                }).AddDebug();
+            });
+        }
+
+        // Placeholder for configuration initialization
+    }
+}
